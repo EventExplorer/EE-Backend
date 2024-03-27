f52c4e79926da60b4459337ada7a0b84b6b63704
somasekhar2356 <vdarisi@albany.edu>
2024-03-27 17:23:15 -0400

    updated backend structure

---
+++ src/main/java/com/ee/eventex/EventExApplication.java
@@ -0,0 +1,13 @@
+package com.ee.eventex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ee/eventex/EventExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ee.eventex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
