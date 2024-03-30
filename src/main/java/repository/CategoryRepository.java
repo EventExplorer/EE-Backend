0f03d83673f233c56d522d65c5b4f0044621f184
Venkata Vinay Thota <vthota@albany.edu>
2024-03-30 17:36:02 -0400

    creating category repository

---
+++ src/main/java/repository/CategoryRepository.java
@@ -0,0 +1,18 @@
+package repository;
+
+import com.ee.entities.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Integer> {
+
+    @Query("SELECT c.id FROM CategoryEntity c WHERE c.name = :name")
+    Optional<CategoryEntity> findIdByName(@Param("name") String name);
+
+
+
+}
